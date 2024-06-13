1d5283565c0fe00045cbcc4b61fde85da0ed325e
Malte PC <malteaschenbach@gmail.com>
2024-06-13 16:42:05 +0200

    missed some stuff in previous commit

--- src/custom/mod.rs
+++ src/custom/mod.rs
@@ -1,6 +1,7 @@
-
 mod checkbox;
 mod options;
+mod input;
 
 pub use checkbox::checkbox;
 pub use options::options;
+pub use input::input;
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,12 @@
+use colored::Colorize;
+
 mod custom;
 mod ascii;
 mod unicode;
 
 
 fn main() {
+    let s = custom::input("Enter your name\n> ".bold(), |input| !input.is_empty());
     let x = unicode::checkbox(&vec!["a", "b", "c", "d", "e"]);
     let y = unicode::options(&vec!["a", "b", "c", "d", "e"]);
 }
